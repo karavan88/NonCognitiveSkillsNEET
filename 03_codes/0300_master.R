17a00803f999896f94656d0f535ec971567939c2
Garen Avanesian <karen.avanesyan@gmail.com>
2023-11-10 12:17:01 -0500

    folder structure

---
+++ 03_codes/0300_master.R
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------------
+# Project: Non-Cognitive Skills of NEET
+# Organization: SFedU Future Skills Research Lab
+# Objective: Excecute all the codes of the project to carry out the analysis
+# Author: Garen Avanesian, Valeria Egorova
+# Date: 10 Nov 2023
+#-------------------------------------------------------------------
+
---
+++ 03_codes/0304_psm.R
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------------
+# Project: Non-Cognitive Skills of NEET
+# Organization: SFedU Future Skills Research Lab
+# Objective: Carry out propensity score matching of the data
+# Author: Garen Avanesian, Valeria Egorova
+# Date: 10 Nov 2023
+#-------------------------------------------------------------------
+
